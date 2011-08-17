787c47b5073c0cd12ddc6c11bb2ba521d32cbb61
qishione <sal.su@qq.com>
2011-08-17 17:56:03 +0800

    fir com

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Suzhen::Application.config.secret_token = '5e30d63e76244a7611fd82a93587ac00e4cb8645a7fe66fa9590f367d6f75eb9222478fa85c64c00c75e9620e9149343035fbad23073719e60013106658657b3'
